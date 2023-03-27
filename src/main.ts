f2f3928d0a0bdb83ac0465ab127ada5907bb205d
Ezzat Malak <39204454+ezzatmakar@users.noreply.github.com>
2023-03-27 13:11:35 +0200

    fix swagger imports

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { useContainer } from 'class-validator';
-import cookieParser from 'cookie-parser';
-import { json, urlencoded } from 'body-parser';
-import helmet from 'helmet';
-// TODO: needs to updated
-import { description } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
@@ -17,17 +12,13 @@ async function bootstrap() {
     exclude: [{ path: 'health', method: RequestMethod.GET }],
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.use(cookieParser());
-  app.use(helmet({ contentSecurityPolicy: false }));
-  app.use(json({ limit: '100mb' }));
-  app.use(urlencoded({ limit: '100mb', extended: true }));
   const config = new DocumentBuilder()
-    .setTitle('App Apis')
-    .setDescription(description)
+    .setTitle('Your API Title')
+    .setDescription('Your API Description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
